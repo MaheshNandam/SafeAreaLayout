9ffaa93ef60afb8a0cca2c3a8b6d208547d6ab6b
Mahesh <mahesh@BLRMIL14Mahesh.local>
2018-02-19 16:12:28 +0530

    design changes

--- App.js
+++ App.js
@@ -9,24 +9,53 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  SafeAreaView,
+  StatusBar
 } from 'react-native';
-
-import SafeAreaLayout from './SafeAreaLayout';
 
 export default class App extends Component<{}> {
   render() {
     return (
-      <View style={styles.container}>
-        <SafeAreaLayout />
-      </View>
+      <SafeAreaView forceInset={{bottom: 'always'}} style={styles.container}>
+          <StatusBar barStyle='dark-content' />
+          <View style={styles.body}>
+              <View style={styles.inlineBlock}>
+                  <Text style={styles.welcome}>
+                      border design fixes using <Text style={styles.titleText}>'SafeAreaView'</Text>
+                  </Text>
+              </View>
+          </View>
+      </SafeAreaView>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#000',
+      flex: 1,
+      backgroundColor: '#fff',
+  },
+  body:{
+      paddingHorizontal: 20,
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderColor: 'red',
+      borderWidth: 2,
+  },
+  inlineBlock:{
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      alignItems: 'center',
+  },
+  welcome: {
+      fontSize: 15,
+      color: '#000',
+      marginVertical: 10,
+  },
+  titleText:{
+    fontSize: 15,
+    fontWeight: 'bold'
   }
 });
---
+++ Router.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { View, Text, Dimensions } from 'react-native';
+import App from './App';
+
+export default class Router extends Component {
+  render(){
+    return <App/>;
+  }
+};
